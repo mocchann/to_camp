a760022c82e613cc410aca213e9d8535df65b236
もっちゃん <s7056i3056@gmail.com>
2023-02-02 23:20:09 +0900

    キャンプ場基本情報テーブルと周辺施設テーブルの中間テーブルを作成した

---
+++ src/database/migrations/2023_02_02_231513_create_campground_information_around_campsite_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('campground_information_around_campsite', function (Blueprint $table) {
+            $table->id();
+            $table->bigIncrements('campground_informations_id');
+            $table->bigIncrements('around_campsites_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('campground_information_around_campsite');
+    }
+};
