9963913614b29f2a7d39a847713f0778d21e4ecb
もっちゃん <s7056i3056@gmail.com>
2023-02-02 22:49:02 +0900

    周辺施設テーブルを作成した

---
+++ src/database/migrations/2023_02_02_224427_create_surrounding_facilities_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('surrounding_facilities', function (Blueprint $table) {
+            $table->id();
+            $table->string('near_staion');
+            $table->string('near_interchange');
+            $table->string('near_spa');
+            $table->string('near_supermarket');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('surrounding_facilities');
+    }
+};
