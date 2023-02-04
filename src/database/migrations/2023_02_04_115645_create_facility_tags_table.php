03fa3b652eae6cc48ca1e0b71c631638d25f99ac
もっちゃん <s7056i3056@gmail.com>
2023-02-04 18:35:53 +0900

    周辺施設テーブル作成

---
+++ src/database/migrations/2023_02_04_115645_create_facility_tags_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('facility_tags', function (Blueprint $table) {
+            $table->id();
+            $table->boolean('gate');
+            $table->boolean('manager_resident');
+            $table->boolean('manager_on_time');
+            $table->boolean('trash');
+            $table->boolean('coin_shower');
+            $table->boolean('free_shower');
+            $table->boolean('washlet');
+            $table->boolean('flush_toilet');
+            $table->boolean('simple_toilet');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('facility_tags');
+    }
+};
