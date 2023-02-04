100e29eee02ded294df41430620ec13975f94d6a
もっちゃん <s7056i3056@gmail.com>
2023-02-04 18:37:05 +0900

    その他のタグ

---
+++ src/database/migrations/2023_02_04_131035_create_other_tags_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('other_tags', function (Blueprint $table) {
+            $table->id();
+            $table->boolean('is_all_season');
+            $table->boolean('early_checkin');
+            $table->boolean('late_checkout');
+            $table->boolean('is_day_camp');
+            $table->boolean('is_firewood');
+            $table->boolean('is_ten_minute_walk');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('other_tags');
+    }
+};
