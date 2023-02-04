69268040204f6e940ccc074f6b0a8de2f75aea41
もっちゃん <s7056i3056@gmail.com>
2023-02-04 16:13:29 +0900

    山テーブルとの中間テーブルを作成

---
+++ src/database/migrations/2023_02_04_011044_create_campground_information_mountain_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('campground_information_mountain', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('campground_information_id');
+            $table->bigInteger('mountain_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('campground_information_mountain');
+    }
+};
