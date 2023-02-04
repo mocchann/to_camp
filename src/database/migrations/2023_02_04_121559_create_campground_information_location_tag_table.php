e7f2cfb36e2b6ba79140f090cb6cea9259ea7f9f
もっちゃん <s7056i3056@gmail.com>
2023-02-04 18:36:55 +0900

    ロケーションとの中間テーブル

---
+++ src/database/migrations/2023_02_04_121559_create_campground_information_location_tag_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('campground_information_location_tag', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('campground_information_id');
+            $table->unsignedBigInteger('location_tag_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('campground_information_location_tag');
+    }
+};
