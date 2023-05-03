a5ee6daf9beec5b1d2583f568f3ec8c7aa258407
もっちゃん <s7056i3056@gmail.com>
2023-05-03 13:17:24 +0900

    不要な記述削除

--- src/database/migrations/2014_10_12_000000_create_users_table.php
+++ src/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name', 100);
+            $table->string('name');
             $table->string('email');
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password', );
+            $table->string('password');
             $table->rememberToken();
             $table->timestamps();
 
