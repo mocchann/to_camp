03e7660680595644f7ac2fa1d260895b6f1386b9
もっちゃん <s7056i3056@gmail.com>
2023-05-13 18:34:51 +0900

    不要な記述を削除

--- src/database/seeders/EarlyCheckInSeeder.php
+++ src/database/seeders/EarlyCheckInSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\EarlyCheckIn;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class EarlyCheckInSeeder extends Seeder
