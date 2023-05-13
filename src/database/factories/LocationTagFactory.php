6d3276d89bcf9e9a13efbed226a1814b17811474
もっちゃん <s7056i3056@gmail.com>
2023-05-13 18:29:17 +0900

    tagテーブルのFactoryを作成

---
+++ src/database/factories/LocationTagFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\LocationTag>
+ */
+class LocationTagFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => '山',
+        ];
+    }
+}
