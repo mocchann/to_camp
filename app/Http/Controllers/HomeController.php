e21d061edd7599b85091e3796e1757f744c2b511
もっちゃん <s7056i3056@gmail.com>
2024-05-28 23:43:00 +0900

    change: コメントアウト外した

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -17,16 +17,16 @@ class HomeController extends Controller
     {
         $campground_data = [];
         $campground_data = Campground::find(3)->animeTags;
-        // $campground_data = [
-        //     'campgrounds' => Campground::all(),
-        //     'day_camps' => DayCamp::all(),
-        //     'early_check_ins' => EarlyCheckIn::all(),
-        //     'late_check_outs' => LateCheckOut::all(),
-        //     'section_sites' => SectionSite::all(),
-        //     'free_sites' => FreeSite::all(),
-        //     'reviews' => Review::all(),
-        //     'bookmarks' => Bookmark::all(),
-        // ];
+        $campground_data = [
+            'campgrounds' => Campground::all(),
+            'day_camps' => DayCamp::all(),
+            'early_check_ins' => EarlyCheckIn::all(),
+            'late_check_outs' => LateCheckOut::all(),
+            'section_sites' => SectionSite::all(),
+            'free_sites' => FreeSite::all(),
+            'reviews' => Review::all(),
+            'bookmarks' => Bookmark::all(),
+        ];
 
         return view('home.index', ['campground_data' => $campground_data]);
     }
